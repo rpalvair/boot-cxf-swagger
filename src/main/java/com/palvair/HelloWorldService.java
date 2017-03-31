8ae5825e317562b8486b299e655904e6da7a1eda
widdy <r.palvair@gmail.com>
2017-03-31 22:48:18 +0200

    refactor : placer les annotations dans l'implémentation

--- src/main/java/com/palvair/HelloWorldService.java
+++ src/main/java/com/palvair/HelloWorldService.java
@@ -1,18 +1,6 @@
 package com.palvair;
 
-import org.springframework.stereotype.Service;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-@Service
-@Path("/sayHello")
 public interface HelloWorldService {
-    @GET
-    @Path("/{a}")
-    @Produces(MediaType.TEXT_PLAIN)
-    String sayHello(@PathParam("a") String a);
+
+    String sayHello(final String a);
 }
--- src/main/java/com/palvair/HelloWorldServiceImpl.java
+++ src/main/java/com/palvair/HelloWorldServiceImpl.java
@@ -1,7 +1,15 @@
 package com.palvair;
 
 import io.swagger.annotations.*;
+import org.springframework.stereotype.Service;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Service
+@Path("/sayHello")
 @Api(value = "/sayHello", description = "Hello world")
 public class HelloWorldServiceImpl implements HelloWorldService {
 
@@ -16,6 +24,9 @@ public class HelloWorldServiceImpl implements HelloWorldService {
                     response = String.class
             )}
     )
+    @GET
+    @Path("/{a}")
+    @Produces(MediaType.TEXT_PLAIN)
     @Override
     public String sayHello(@ApiParam(name = "test", value = "test") String a) {
         return "Hello " + a + ", Welcome to CXF RS Spring Boot World!!!";
